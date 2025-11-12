19d867a1c194ed253fe66c0d119c8c202fc51ded
Charls <charls@gmail.com>
2025-11-12 13:30:48 +0530

    new file

---
+++ www.mm
@@ -0,0 +1,8 @@
+🌿 Branching (Working on Features)
+Command	Purpose
+git branch	Show all branches
+git branch <name>	Create a new branch
+git checkout <name>	Switch to another branch
+git checkout -b <name>	Create and switch to a new branch
+git merge <name>	Merge branch into current branch
+git branch -d <name>	Delete a branchgig
